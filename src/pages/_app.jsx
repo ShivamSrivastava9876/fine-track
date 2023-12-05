2da41b855b71d09f38ba3979e51bf48d818f5971
Shivam Srivastava <9876srivastavashiavm@gmail.com>
2023-12-05 13:10:52 +0530

    Uninstalled react alert

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,24 +1,15 @@
 import '@/styles/globals.css'
 import { Providers } from '../redux/provider'
-import { transitions, positions, Provider as AlertProvider } from 'react-alert'
-import AlertTemplate from 'react-alert-template-basic'
-import { store } from '@/redux/store'
+// import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+// import AlertTemplate from 'react-alert-template-basic'
+// import { store } from '@/redux/store'
 
-const options = {
-  // you can also just use 'bottom center'
-  position: positions.BOTTOM_CENTER,
-  timeout: 5000,
-  offset: '30px',
-  // you can also just use 'scale'
-  transition: transitions.SCALE
-}
+
 
 export default function App({ Component, pageProps }) {
   return (
 
-    <AlertProvider template={AlertTemplate} {...options}>
       <Providers><Component {...pageProps} /></Providers>
-    </AlertProvider>
 
   )
 }
