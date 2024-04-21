4976e7b45a3a23074463aae4eab9742675e5bedb
Shivam Srivastava <9876srivastavashiavm@gmail.com>
2024-04-21 23:38:36 +0530

    Fixed issues in edit form of product

--- src/components/ViewPhotosOfProduct.jsx
+++ src/components/ViewPhotosOfProduct.jsx
@@ -22,7 +22,7 @@ const ViewPhotosOfProduct = ({ rows, selectedProductImages, openImage, setOpenIm
             <div className={`fixed inset-0 z-40 bg-black opacity-10 backdrop-blur-md transition-opacity ${modalClasses}`}></div>
             <div className="flex justify-center items-center fixed bottom-2 top-2 right-5 left-5 z-50">
 
-                <div className="p-8 rounded shadow-md flex flex-col justify-start overflow-y-scroll scroll-smooth no-scrollbar h-full w-auto bg-white border border-blue-500">
+                <div className="p-8 rounded shadow-md relative flex flex-col justify-start overflow-y-scroll scroll-smooth no-scrollbar h-full w-auto bg-white border border-blue-500">
                     <button onClick={handleCancel} className="absolute top-2 right-2 text-gray-500 hover:text-gray-800">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
